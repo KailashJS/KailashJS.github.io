f5797fb1bddfa3bbbdbe31186e629847dbb21c26
Kailash Chandra Bhakta <kcbhakta.99@gmail.com>
2023-02-05 19:53:35 +0530

    feat: major changes in UI - fonts, and card styles and some hover effects refactored

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ function setTheme(mode) {
   if (mode === "light") {
     document.getElementById("theme-style").href = "default.css";
     document.getElementById('github').style.fill="black";
-    document.getElementById('codesandbox').style.fill="black";
-    document.getElementById('codepen').style.fill="black";
+    // document.getElementById('codesandbox').style.fill="black";
+    // document.getElementById('codepen').style.fill="black";
   }
 
   if (mode === "blue") {
     document.getElementById("theme-style").href = "blue.css";
     document.getElementById('github').style.fill="white";
-    document.getElementById('codesandbox').style.fill="white";
-    document.getElementById('codepen').style.fill="white";
+    // document.getElementById('codesandbox').style.fill="white";
+    // document.getElementById('codepen').style.fill="white";
   }
 
   if (mode === "green") {
     document.getElementById("theme-style").href = "green.css";
     document.getElementById('github').style.fill="black";
-    document.getElementById('codesandbox').style.fill="black";
-    document.getElementById('codepen').style.fill="black";
+    // document.getElementById('codesandbox').style.fill="black";
+    // document.getElementById('codepen').style.fill="black";
   }
 
   if (mode === "purple") {
     document.getElementById("theme-style").href = "purple.css";
     document.getElementById('github').style.fill="white";
-    document.getElementById('codesandbox').style.fill="black";
-    document.getElementById('codepen').style.fill="white";
+    // document.getElementById('codesandbox').style.fill="black";
+    // document.getElementById('codepen').style.fill="white";
   }
   localStorage.setItem("theme", mode);
 }
@@ -33,7 +33,8 @@ let theme = localStorage.getItem("theme");
 
 // Fetching the theme from the local storage if available
 if (theme) setTheme(theme);
-else setTheme("light");
+// else setTheme("light");
+else setTheme("purple");
 
 let themeDots = document.getElementsByClassName("theme-dot");
 
