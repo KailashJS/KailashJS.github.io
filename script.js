6008bc7e9ff261c13681d0595139d8dcc22db794
KailashJS <kcbhakta.99@gmail.com>
2023-03-21 07:28:47 +0530

    feature: portfolio with updated resume updated

--- script.js
+++ script.js
@@ -147,12 +147,28 @@ class Project {
 
 const projects = [
   {
+    projectName: "Lavish React",
+    projectDescription:
+      "Created a website for the fictional brand Lavish with React. With Responsive Material design and reusable components.",
+    projectImage: "images/Lavish-react.png",
+    codeLink: "https://github.com/KailashJS/lavish-react-website/",
+    demoLink: "https://lavish-react.web.app/"
+  },
+  {
     projectName: "Redify Inc.",
     projectDescription:
       "Designed & developed the landing page for Redify Inc. with React.js (Freelancing project on Fiver).",
     projectImage: "images/redify-landing-page.jpg",
     codeLink: "https://github.com/KailashJS/website-redify/",
     demoLink: "https://redifyinc.web.app/"
+  },
+  {
+    projectName: "Movie App - KailashJS",
+    projectDescription:
+      "The application is a movie database that allows users to search for information about movies.",
+    projectImage: "images/movie-app-kailashjs.jpg",
+    codeLink: "https://github.com/KailashJS/movie-app-kailashjs",
+    demoLink: "https://movieapp.kailashjs.com/"
   },
   // {
   //   projectName: "React Framer",
@@ -163,20 +179,36 @@ const projects = [
   //   demoLink: "https://react-framer-demo.web.app/"
   // },
   {
+    projectName: "Intelligent Booking Engine (Bookingjini Pvt Ltd.)",
+    projectDescription:
+      "Intelligent Booking Engine developed at Bookingjini : The beauty of complex frontend architechture.",
+    projectImage: "images/Bookingengine-bookingjini.jpg",
+    codeLink: "https://kailashjs.substack.com/p/intelligent-booking-engine-developed",
+    demoLink: "https://booking.hotelgoldentree.in/property"
+  },
+  {
+    projectName: "Extranet V4 (Bookingjini Pvt Ltd.)",
+    projectDescription:
+      "Team of 12 engineers worked relentlessly for a year in quest to develop Extranet V4 - a more powerful, beautiful and informative version.",
+    projectImage: "images/heritance-extranetv4.png",
+    codeLink: "https://kailashjs.substack.com/p/extranet-v4-one-of-the-major-upgradations",
+    demoLink: "https://www.youtube.com/watch?v=HOkCGxmSxp4&t=26s/"
+  },
+  {
+    projectName: "Hotel-chains and Intranet (Bookingjini Pvt Ltd.)",
+    projectDescription:
+      "Hotel-chains and intranet : A powerful tool developed by Bookingjini to monitor and manage multiple hotels.",
+    projectImage: "images/Intranet-bookingjini.jpg",
+    codeLink: "https://kailashjs.substack.com/p/hotel-chains-and-intranet-a-powerful",
+    demoLink: "https://www.youtube.com/watch?v=zcBs5iabs1w&t=235s"
+  },
+  {
     projectName: "Portfolio",
     projectDescription:
       "Designed & Built the Portfolio using HTML CSS and Vanilla JavaScript to show case the Professional Journey.",
     projectImage: "images/new-portfolio.png",
     codeLink: "https://github.com/KailashJS/kailash-portfolio/",
     demoLink: "http://kailashjs.github.io/"
-  },
-  {
-    projectName: "Lavish React",
-    projectDescription:
-      "Created a website for the fictional brand Lavish with React. With Responsive Material design and reusable components.",
-    projectImage: "images/Lavish-react.png",
-    codeLink: "https://github.com/KailashJS/lavish-react-website/",
-    demoLink: "https://lavish-react.web.app/"
   },
   {
     projectName: "Photo Album",
@@ -202,14 +234,18 @@ const projects = [
     codeLink: "https://github.com/KailashJS/the-healthy-elderly",
     demoLink: "https://the-healthy-elderly.web.app/"
   },
-  {
-    projectName: "Dashboard for Redify Inc.",
-    projectDescription:
-      "Designed and developed UI/frontend for portal/Dashboard for Redify users with React.js",
-    projectImage: "images/Redify.jpg",
-    codeLink: "https://github.com/KailashJS/portal-redify",
-    demoLink: "https://portal-redifyinc.web.app/"
-  },
+
+
+
+
+  // {
+  //   projectName: "Dashboard for Redify Inc.",
+  //   projectDescription:
+  //     "Designed and developed UI/frontend for portal/Dashboard for Redify users with React.js",
+  //   projectImage: "images/Redify.jpg",
+  //   codeLink: "https://github.com/KailashJS/portal-redify",
+  //   demoLink: "https://portal-redifyinc.web.app/"
+  // },
   // {
   //   projectName: "Pulse graph",
   //   projectDescription:
